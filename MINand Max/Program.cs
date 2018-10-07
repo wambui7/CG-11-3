9172f2a9d430a8e92513c86586efaac7ebfd4f19
sheila wambui <shiko.sheila@gmail.com>
2018-10-07 17:42:49 -0500

    Add project files.

---
+++ MINand Max/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MINand_Max
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
